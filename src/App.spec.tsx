77f0e4f2bceef969be651c02b03cb59855fcc878
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-13 21:46:08 -0500

    ayo

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,10 +2,16 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import App from "./App.js";
+import React from "react";
 
 describe("App", () => {
   it("renders the right thing in the dom", () => {
-    render(<App />);
+    // NOTE: does strict mode do anything in test?
+    render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
 
     // TODO: see if 2 renders in development mode
     expect(screen.getByText("2")).toBeInTheDocument();
